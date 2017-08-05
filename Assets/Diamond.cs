52c873c6ac11417838ecdc22f07c6f7528feaa4a
james7132 <james.siqi.liu@gmail.com>
2017-08-05 14:45:41 -0700

    Add basic diamon collider support

---
+++ Assets/Diamond.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace HouraiTeahouse.SmashBrew {
+
+    [Serializable]
+    public struct Diamond {
+
+        const float yOffset = 0.0005f;
+
+        public Vector2 Center;
+        public Vector2 Size;
+
+        public Vector2 Extents {
+             get { return Size / 2; }
+        }
+
+        public Vector2[] Points {
+            get {
+                var extents = Extents;
+                return new Vector2[] {
+                    Center + extents.y * Vector2.up,
+                    Center + extents.x * Vector2.right,
+                    Center - extents.y * Vector2.up,
+                    Center - extents.x * Vector2.right
+                };
+            }
+        }
+
+        public Diamond PhysicsCast(Vector2 direction) {
+            var points = Points;
+            var baseDistance = direction.magnitude;
+            var distance = baseDistance;
+            var newCenter = Center + direction;
+            for (var i = 0; i < points.Length; i++) {
+                var offset = points[i] - Center;
+                var internalRaycast = Physics.RaycastAll(Center, offset, offset.magnitude);
+                var externalRaycast = Physics.RaycastAll(points[i], direction, baseDistance);
+                if (internalRaycast.Length <= 0 && externalRaycast.Length <= 0)
+                    continue;
+                foreach(var hit in internalRaycast.Concat(externalRaycast)) {
+                    if (hit.distance > distance)
+                        continue;
+                    newCenter = (Vector2)hit.point - offset + (yOffset * Vector2.up);
+                    distance = hit.distance;
+                    Log.Debug("{0} {1} {2}", Center, offset, newCenter);
+                }
+            }
+            var copy = this;
+            copy.Center = newCenter;
+            return copy;
+        }
+
+        public void Draw(Color? color = null) {
+            Color oldColor = Gizmos.color;
+            if (color != null)
+                Gizmos.color = color.Value;
+            var corners = Points;
+            for (var i = 0; i < corners.Length; i++)
+                Gizmos.DrawLine(corners[i], corners[(i + 1) % corners.Length]);
+            if (color != null)
+                Gizmos.color = oldColor;
+        }
+
+        public void DrawConnects(Diamond target, Color? color = null) {
+            Color oldColor = Gizmos.color;
+            if (color != null)
+                Gizmos.color = color.Value;
+            var c1 = Points;
+            var c2 = target.Points;
+            for (var i = 0; i < c1.Length; i++)
+                Gizmos.DrawLine(c1[i], c2[i]);
+            if (color != null)
+                Gizmos.color = oldColor;
+        }
+        
+    }
+
+}
---
+++ Assets/SmashCharacterCollider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace HouraiTeahouse.SmashBrew {
+
+    public class SmashCharacterCollider : MonoBehaviour {
+
+        [SerializeField]
+        Diamond _collider;
+
+        Diamond _prevFrame;
+        Diamond _prediction;
+        Diamond _currentFrame;
+
+        [SerializeField]
+        Vector2 _movement;
+
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// </summary>
+        void Awake() {
+        }
+
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// </summary>
+        void Update() {
+            _prevFrame = TransformCollider(_collider);
+            Vector3 diff = _movement * Time.deltaTime;
+            _currentFrame = _prevFrame.PhysicsCast(diff);
+            transform.position += (Vector3)(_currentFrame.Center - _prevFrame.Center);
+            _prediction = _currentFrame.PhysicsCast(diff);
+        }
+
+        Diamond TransformCollider(Diamond collider) {
+            collider.Center = transform.TransformPoint(collider.Center);
+            return collider;
+        }
+
+        public void Move(Vector2 direction) {
+            _movement += direction;
+        }
+
+        /// <summary>
+        /// Callback to draw gizmos that are pickable and always drawn.
+        /// </summary>
+        void OnDrawGizmos() {
+#if UNITY_EDITOR
+            if (EditorApplication.isPlayingOrWillChangePlaymode) {
+                _currentFrame.DrawConnects(_prevFrame, Color.yellow);
+                _currentFrame.DrawConnects(_prediction, Color.grey);
+                _prevFrame.Draw(Color.red);
+                _prediction.Draw(Color.blue);
+                _currentFrame.Draw(Color.green);
+            } else {
+                TransformCollider(_collider).Draw(Color.green);
+            }
+#endif
+        }
+
+    }
+
+}
