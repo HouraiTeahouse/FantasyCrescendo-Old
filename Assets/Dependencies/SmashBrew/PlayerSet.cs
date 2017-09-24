e68c744e5d7a2aae8daa92e96a5b970820b9ceb1
james7132 <james.siqi.liu@gmail.com>
2017-09-24 16:26:50 -0700

    Fix PlayerManager null issues

---
+++ Assets/Dependencies/SmashBrew/PlayerSet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace HouraiTeahouse.SmashBrew {
+
+    public class PlayerSet : IEnumerable<Player> {
+
+        Player[] _players;
+
+        public Player Get(int id) {
+            if(!Check.Range(id, _players))
+                throw new ArgumentOutOfRangeException();
+            Assert.IsTrue(id < GameMode.Current.MaxPlayers);
+            return _players[id];
+        }
+
+        public IEnumerator<Player> GetEnumerator() => _players.Cast<Player>().GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public int Count => _players.Length;
+
+        public PlayerSet() {
+            // Note: These objects are not intended to be destroyed and thus do not unresgister these event handlers
+            // If there is a need to remove or replace them, this will need to be changed.
+            GameMode.OnRegister += mode => RebuildPlayerArray();
+            GameMode.OnChangeGameMode += mode => RebuildPlayerArray();
+            _players = new Player[0];
+            RebuildPlayerArray();
+        }
+
+        void RebuildPlayerArray() {
+            var maxPlayers= !GameMode.All.Any() ? 0 : GameMode.All.Max(mode => mode.MaxPlayers);
+            if (maxPlayers <= Count)
+                return;
+            var temp = new Player[maxPlayers];
+            if(_players != null)
+                Array.Copy(_players, temp , _players.Length);
+            for (var i = 0; i < temp.Length; i++) {
+                if(temp[i] == null)
+                    temp[i] = new Player(i);
+            }
+            _players = temp;
+        }
+
+        public void ResetAll() {
+            var blankSelection = new PlayerSelection();
+            foreach (Player player in _players) {
+                if (player == null)
+                    continue;
+                player.Selection = blankSelection;
+                player.Type = PlayerType.None;
+            }
+        }
+
+    }
+
+}
