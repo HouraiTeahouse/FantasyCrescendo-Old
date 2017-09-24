b2125c2f90e9517d9f2cd59603c48f37f9cc7b54
james7132 <james.siqi.liu@gmail.com>
2017-09-24 15:14:04 -0700

    Add doc comments for AsyncOperationExtensions.ToTask

--- Assets/Dependencies/HouraiLib/Extensions/AsyncOperationExtensions.cs
+++ Assets/Dependencies/HouraiLib/Extensions/AsyncOperationExtensions.cs
@@ -6,6 +6,12 @@ namespace HouraiTeahouse {
 
     public static class AsyncOperationExtensions {
 
+        /// <summary>
+        /// Converts an existing AsyncOperation into a ITask.
+        /// Can be used to simplify working with async Unity3D APIs.
+        /// </summary>
+        /// <param name="operation">the async operation</param>
+        /// <returns>A task that will be resolved when the operation is complete.</returns>
         public static ITask<T> ToTask<T>(this T operation) where T : AsyncOperation {
             var  task = new Task<T>();
             operation.completed += (_) => task.Resolve(operation);
