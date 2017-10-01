bfa87feaee47d77dff8d847f9d5f66b6a1c5c28b
james7132 <james.siqi.liu@gmail.com>
2017-10-01 01:58:38 -0700

    Add TrainingMatch prototype

---
+++ Assets/Dependencies/SmashBrew/Match/Rules/TrainingMatch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HouraiTeahouse.SmashBrew.Matches {
+
+    public class TrainingMatch : MatchRule {
+
+        protected override bool CheckActive(MatchConfig config) => false;
+
+        protected override void OnInitialize(MatchConfig config) {
+            var eventManager = Mediator.Global;
+            var context = eventManager.CreateUnityContext(this);
+            // ALways respawn players
+            context.Subscribe<PlayerDieEvent>(args => 
+                eventManager.Publish(new PlayerRespawnEvent {
+                    Player = args.Player
+                }));
+        }
+
+    }
+
+}
